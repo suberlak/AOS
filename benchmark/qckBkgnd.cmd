8de27d0e91ec13c0f07a2a7316ab9e9f1fc7b65d
suberlak <sci.chris@gmail.com>
2021-05-06 02:37:12 +0000

    add cmd files for benchmark runs

---
+++ benchmark/qckBkgnd.cmd
@@ -0,0 +1,12 @@
+quickbackground
+raydensity 0
+perturbationmode 1
+trackingmode 0
+cleartracking
+clearclouds
+lascatprob 0.0
+contaminationmode 0
+diffractionmode 1
+straylight 0
+detectormode 0
+centroidfile 1
