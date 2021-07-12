c058696740e3bb6823eea7c0060d4d1df6b4764b
suberlak <sci.chris@gmail.com>
2021-07-12 18:42:08 +0000

    add update Euler angle

--- update_focalplanelayout_lsst.py
+++ update_focalplanelayout_lsst.py
@@ -7,6 +7,15 @@ from lsst.obs.lsst import LsstCam
 
 camera = LsstCam().getCamera()
 
+# read all the lines to a list.
+# as a base for all changes is the copy of segmentation.txt from before I started 
+# to change anything that had to do with orientation,
+# i.e. as of v1.0.4 https://github.com/lsst-ts/phosim_syseng4/releases/tag/v1.0.4 
+
+# The geometry update for LSSTCam was https://github.com/lsst-ts/phosim_syseng4/releases/tag/v1.0.5
+# The geometry update for LSSTComCam was https://github.com/lsst-ts/phosim_syseng4/releases/tag/v1.0.6
+# Fix gain, variance for LSSTComCam https://github.com/lsst-ts/phosim_syseng4/releases/tag/v1.0.7 
+
 
 ###########################################
 #                                         # 
@@ -29,6 +38,18 @@ for line in contentLinesZer:
 # read the file and split into two lists
 headerLines, contentLines = up.readFile('/project/scichris/aos/phosim_syseng4/data/lsst/focalplanelayout_old.txt')
 
+
+# UPDATE EULER ANGLE? 
+update_euler = True
+
+if update_euler:
+    print('Updating Euler angle ')
+    ticket_number = 'DM-30367' # both: obs_lsst orientation
+else:
+    print('Keeping  original Euler angle')
+    ticket_number = 'DM-28557' # lsstCam 
+    
+    
 # update the content 
 newContentLines = []
 for line in contentLines:
@@ -74,6 +95,13 @@ for line in contentLines:
     newSplitContent[4] = str(xnew)
     newSplitContent[5] = str(ynew)
     
+    # Update Euler angles 
+    if update_euler:
+        eulerAngle1 = float(splitContent[12])
+        newEulerAngle1 = eulerAngle1+90
+        newSplitContent[12] = str(newEulerAngle1)
+
+
     # update the dx, dy - since we know the actual position, set these to 0 ...
     dx,dy,dz = float(splitContent[15]), float(splitContent[16]), float(splitContent[17])
     newSplitContent[15] = '0' # <-- set dx to 0 
@@ -90,7 +118,7 @@ for line in contentLines:
     newContentLines.append(' '.join(newSplitContent)+'\n')
     
 # store the updated version: the header and  updated  content lines 
-fname = "/project/scichris/aos/phosim_syseng4/data/lsst/focalplanelayout_upd.txt"
+fname = f"/project/scichris/aos/phosim_syseng4/data/lsst/focalplanelayout_{ticket_number}.txt"
 f = open(fname, "w")
 f.writelines(headerLines)
 f.writelines(newContentLines)
