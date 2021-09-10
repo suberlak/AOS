4eb8f5a638f0b3321aef9490aaadaca161daba86
suberlak <suberlak@uw.edu>
2021-09-09 20:15:35 -0500

    add plot_butler_with_wcs

--- run_ps1_functions.py
+++ run_ps1_functions.py
@@ -7,9 +7,18 @@ import numpy as np
 
 from lsst.obs.base import createInitialSkyWcsFromBoresight
 from lsst.afw.cameraGeom import FOCAL_PLANE, PIXELS
-#import lsst.obs.lsst as obs_lsst
 from lsst.obs.lsst import LsstCam, LsstComCam
 import lsst.geom
+
+
+from lsst.daf import butler as dafButler
+import lsst.afw.cameraGeom.utils as cameraGeomUtils
+import lsst.afw.display as afwDisplay
+from astropy.visualization import ZScaleInterval
+
+import matplotlib.pyplot as plt
+from matplotlib import rcParams 
+import pandas as pd
 # this is a place to put all functions 
 # used by at least two of the 
 # ps1-simulation suite,
@@ -20,11 +29,75 @@ import lsst.geom
 # run_ps1_slurm
 
 
-from lsst.obs.base import createInitialSkyWcsFromBoresight
-from lsst.afw.cameraGeom import FOCAL_PLANE, PIXELS
-import lsst.obs.lsst as obs_lsst
-import lsst.geom
+def plot_butler_with_wcs(repo_dir, detector='R22_S10', 
+                         inst_file ='/project/scichris/aos/images/lsstCam/lsstCam_entire_tiled_LSST.inst',
+                        xmax=4000, ymax=4000, coords='CCS'):
+    zscale = ZScaleInterval()
 
+    instrument = 'LSSTCam'
+    #repo_dir = '/project/scichris/aos/images/lsstCam/letter_lsstCam_entire_tiled_LSST_all_rot/DATA/'
+    butler = dafButler.Butler(repo_dir, collections=[f'{instrument}/raw/all',
+                                                     f'{instrument}/calib','run1',
+                                                     f'{instrument}/calib/unbounded'],
+                                 )
+
+    dataId0 = dict(instrument=instrument)
+    dataset = next(iter(butler.registry.queryDatasets(
+                        datasetType='raw',
+                        dataId=dataId0  )))
+
+    expN = dataset.dataId["exposure"]
+
+    exposure = butler.get('postISRCCD', detector=detector, 
+                          instrument=instrument,exposure=expN,
+              collections=['run1'])
+    detector  = exposure.getDetector()
+    data = exposure.image.array
+
+    vmin, vmax = zscale.get_limits(data)
+
+    fig = plt.figure(figsize=(12, 12))
+
+    # plot in DVCS 
+    if coords == 'CCS':
+        data = data.T
+    # otherwise,  for coords = 'DVCS' nothing needs be done 
+    
+    plt.imshow(data, origin='lower', vmin=vmin,vmax=vmax,cmap='gray')
+    plt.colorbar()
+    plt.xlabel('x [px]')
+    plt.ylabel('y [px]')
+
+    plt.title(f'{instrument}, {detector.getName()} ({detector.getSerial()[:3]}), {coords}')
+
+
+    # read in the input instance catalog 
+    # that is the reference catalog 
+    cat = np.genfromtxt(inst_file,
+                            skip_header=16)
+
+    cat_df = pd.DataFrame(cat[:, 1:5], columns=['id', 'ra', 'dec', 'g'])
+
+    # get the wcs 
+    wcs = exposure.getWcs()
+
+    # calculate the position given the x,y 
+    x,y= wcs.skyToPixelArray(cat_df['ra'], cat_df['dec'],degrees=True)
+    
+    if coords == 'CCS':
+        x,y=y,x
+        
+    # plot the WCS
+    plt.scatter(x,y,  facecolors='none', edgecolors='r', marker='o', s=50, label='phoSim WCS')
+
+    plt.xlim(0,ymax)
+    plt.ylim(0,xmax)
+    plt.legend(fontsize=18)
+    detectorName, detectorType = detector.getName(), detector.getSerial()[:3] 
+    #plt.savefig(f'letter_lsstCam_entire_tiled_LSST_all_rot{detectorName}_{detectorType}.jpeg',
+    #           bbox_inches='tight', dpi=150)
+    
+    
 
 def ccd_xy_to_radec(x_px=[0], y_px=[0], boresight_ra=0, boresight_dec=0, rotskypos=0,
                      sensorNameList = ['R22_S11']):
@@ -33,8 +106,8 @@ def ccd_xy_to_radec(x_px=[0], y_px=[0], boresight_ra=0, boresight_dec=0, rotskyp
 
     rotSkyPos=rotskypos
     # get all sensors from the LsstCam : 
-    camera = obs_lsst.lsstCamMapper.LsstCamMapper().camera
-
+    #camera = obs_lsst.lsstCamMapper.LsstCamMapper().camera
+    camera = LsstCam().getCamera()
     #setObsMetaData in bsc/WcsSol.py 
     boresightPointing = lsst.geom.SpherePoint(boresight_ra, 
                                               boresight_dec, 
@@ -448,6 +521,33 @@ def get_field_ra_dec():
                                   }
     return gt_dict
 
+def pixel_letter_F(xmin=500,ymin=200,width=800,height=1200,xspacing=220,yspacing=200):
+    xmax = xmin+width
+    ymax = ymin+height
+    # initialize array storing coords for all pixels 
+    xPx = np.zeros(0)
+    yPx = np.zeros(0)
+
+    # middle horizontal line
+    x_mid = np.arange(xmin,xmax,xspacing)
+    y_mid = np.ones_like(x_mid) * ((ymin+ymax)/2.)
+    xPx = np.append(xPx, x_mid)
+    yPx = np.append(yPx, y_mid)
+    
+    # top horizontal line
+    x_top = x_mid.copy()
+    y_top = np.ones_like(x_top)*ymax
+    xPx = np.append(xPx, x_top)
+    yPx = np.append(yPx, y_top)
+
+    # vertical line
+    y_vert = np.arange(ymin+200,ymax-100,yspacing)
+    x_vert = np.ones_like(y_vert)*xmin
+    xPx = np.append(xPx, x_vert)
+    yPx = np.append(yPx, y_vert)
+
+    return xPx, yPx
+
 
 def write_phosim_header(
     output,
@@ -482,100 +582,130 @@ def write_phosim_header(
         output.write("move 10  1500.0000\n")  
     output.write("camconfig {}\n".format(camconfig))
     
+
+
+# NB:  here we only do 0.25 sec exposure to quickly test if the image is properly made 
+# in generating large catalogs, change to 15 sec. 
+def writePhosimHeader(output, ra, dec, rotskypos=0, rottelpos=0, 
+                      camconfig=3, Opsim_filter=3, mjd=59580., exposure=0.25,
+                     obsid=9006002, position='focal',
+                     opsim_rawseeing=0.69):
+    '''Write to file handle the phosim header'''
+    output.write("Opsim_obshistid {}\n".format(obsid))
+    output.write("Opsim_filter {}\n".format(Opsim_filter))
+    output.write("mjd {}\n".format(mjd))
+    output.write("SIM_SEED {}\n".format(1000))
+    output.write("rightascension {}\n".format(ra))
+    output.write("declination {}\n".format(dec))
+    output.write("rotskypos {}\n".format(rotskypos))
+    output.write("rottelpos {}\n".format(rottelpos))
+    output.write("SIM_VISTIME {}\n".format(exposure))
+    output.write("SIM_NSNAP {}\n".format(1))
+    output.write("moonphase {}\n".format(0.0))
+    output.write("moonalt {}\n".format(-90))
+    output.write("sunalt {}\n".format(-90))
+    output.write("Opsim_rawseeing {}\n".format(opsim_rawseeing))
+    if position == 'extra':
+        output.write("move 10 -1500.0000\n")  # write the defocal movement
+    elif position == 'intra':
+        output.write("move 10  1500.0000\n")  
+    output.write("camconfig {}\n".format(camconfig))
+
+def writePhosimInstFile(catalog, ra, dec, rotskypos=0,  phosimFile="stars.inst", 
+                        passband='r', outDir="./", camconfig=3, opd=False,
+                        mjd=59580,exposure=.25, obsid=9006002, position='focal',
+                        constMag=15. ):
+    '''Generate a phosim instance catalog from a Pandas dataframe or Astropy Table '''
     
-
-def pixel_letter_F(xmin=500,ymin=200,width=800,height=1200,xspacing=220,yspacing=200):
-    xmax = xmin+width
-    ymax = ymin+height
-    # initialize array storing coords for all pixels 
-    xPx = np.zeros(0)
-    yPx = np.zeros(0)
-
-    # middle horizontal line
-    x_mid = np.arange(xmin,xmax,xspacing)
-    y_mid = np.ones_like(x_mid) * ((ymin+ymax)/2.)
-    xPx = np.append(xPx, x_mid)
-    yPx = np.append(yPx, y_mid)
+    passbands = {"u":"umeanpsfmag", 
+                 "g":"gmeanpsfmag", 
+                 "r":"rmeanpsfmag", 
+                 "i":"imeanpsfmag", 
+                 "z":"zmeanpsfmag", 
+                 "y":"ymeanpsfmag", 
+                }
     
-    # top horizontal line
-    x_top = x_mid.copy()
-    y_top = np.ones_like(x_top)*ymax
-    xPx = np.append(xPx, x_top)
-    yPx = np.append(yPx, y_top)
-
-    # vertical line
-    y_vert = np.arange(ymin+200,ymax-100,yspacing)
-    x_vert = np.ones_like(y_vert)*xmin
-    xPx = np.append(xPx, x_vert)
-    yPx = np.append(yPx, y_vert)
-
-    return xPx, yPx
-
-def ccd_xy_to_radec(x_px=[0], y_px=[0], boresight_ra=0, 
-                      boresight_dec=0, rotSkyPos=0,
-                     sensorNameList = ['R22_S11']):
-    # Use the actual elements of SkySim 
-    # https://github.com/lsst-ts/ts_phosim/blob/e25ac907d0385b109caf1ee4a803d9f248b54096/python/lsst/ts/phosim/SkySim.py#L126
-    # and WcsSol , so 
-    # it's more transparent  how CCD coordinates were  
-    # translated  with  SkySim into Ra, Deg catalog 
-
-    # get all sensors from the LsstCam : 
-    #camera = obs_lsst.lsstCamMapper.LsstCamMapper().camera
-    camera = LsstCam().getCamera()
+    outFile = os.path.join(outDir, phosimFile)
     
-    #setObsMetaData in bsc/WcsSol.py 
-    boresightPointing = lsst.geom.SpherePoint(boresight_ra, 
-                                              boresight_dec, 
-                                              lsst.geom.degrees)
-    centerCcd= "R22_S11"
-    skyWcs = createInitialSkyWcsFromBoresight(
-                boresightPointing,
-                (90-rotSkyPos) * lsst.geom.degrees,
-                camera[centerCcd],
-                flipX=False,
-            )
+    filterID = list(passbands.keys()).index(passband)
+    with open(outFile,'w') as output:
+        writePhosimHeader(output, ra, dec, rotskypos=rotskypos, camconfig=camconfig, 
+                          Opsim_filter=filterID, mjd=mjd,
+                         exposure=exposure, obsid=obsid, position=position)
+        if (opd==True):
+            output.write("opd 0 {} {} 500".format(ra,dec))
+        else:
+            for idx in range(len(catalog)):
+                row = catalog[idx]
+                output.write("object {} {} {} {} ../sky/sed_flat.txt 0.0 0.0 0.0 0.0 0.0 0.0 star 12.0 none none\n".format(
+                    int(row["objid"]),row["ra"],row["dec"], constMag))
+    print('Saved as ', outFile)
+    return outFile
+ 
+def plot_cameraGeom(repo_dir = os.path.join('/project/scichris/aos/images/lsstCam/letter_lsstCam_entire_tiledF/DATA/'),
+                   detectorNameList = [ "R04_SW0", "R04_SW1",
+                         "R44_SW0", "R44_SW1",
+                         "R00_SW0", "R00_SW1",
+                         "R40_SW0", "R40_SW1",
+                      
+                       ] ,
+                    instrument = 'LSSTCam',
+                    binSize=16
+                   ):
+    afwDisplay.setDefaultBackend("matplotlib")
 
-    # Get the pixel positions in DM team
-    # pixelDmX, pixelDmY = self._sourProc.camXY2DmXY(xInpixelInCam, yInPixelInCam)
-    # transpose - from DVCS to CCS 
-    xInPixelInCam = x_px
-    yInPixelInCam = y_px
-    pixelDmX, pixelDmY = yInPixelInCam,  xInPixelInCam
+    #repo_dir = os.path.join('/project/scichris/aos/images/lsstCam/letter_lsstCam_entire_tiledF/DATA/')
 
-
-    raList = []
-    decList = []
-    xPxList = []
-    yPxList = []
-    centerChip = camera[centerCcd]
-    for sensorName in  sensorNameList:
-        # same sensorName for each pixel in the list 
-        chipNames =[sensorName for x in range(len(x_px))]
-        for chipX, chipY, ccdX, ccdY, chipName in zip(pixelDmX, 
-                                                      pixelDmY, 
-                                                      xInPixelInCam,
-                                                      yInPixelInCam,
-                                                      chipNames
-                                                     ):
-            #print(chipX,chipY,chipName, raPt,decPt)
-            cameraChip = camera[chipName]
-            # Get x,y on specified detector in terms of mm from center of cam
-            camXyMm = cameraChip.transform(
-                lsst.geom.Point2D(chipX, chipY), PIXELS, FOCAL_PLANE
-            )
-            # Convert mm to pixels
-            camPoint = centerChip.transform(camXyMm, FOCAL_PLANE, PIXELS)
-
-            # Calculate correct ra, dec
-            raPt, decPt = skyWcs.pixelToSky(camPoint)
-
-            raList.append(raPt.asDegrees())
-            decList.append(decPt.asDegrees())
-            xPxList.append(ccdX)
-            yPxList.append(ccdY)
-
-    raInDeg, declInDeg =  np.array([raList, decList])
+    # need to specify the calib collections to be able to access the camera 
     
-    return raInDeg, declInDeg , xPxList, yPxList
+    butler = dafButler.Butler(repo_dir, collections=[f'{instrument}/raw/all',
+                                                     f'{instrument}/calib','run1',
+                                                     f'{instrument}/calib/unbounded']
+                             )
+
+    dataId0 = dict(instrument=instrument)
+    dataset = next(iter(
+                        butler.registry.queryDatasets(
+                            datasetType='postISRCCD', 
+                            collections=['run1'],
+                            dataId=dataId0  )
+                        )
+                   )
+
+    exposure = dataset.dataId["exposure"]
+
+    camera  = butler.get("camera", instrument=instrument, exposure=exposure)
+
+    fig = plt.figure(figsize=(15,15))
+    fig.subplots_adjust(hspace=0., wspace=0.0, )
+
+    ax = fig.add_subplot(1, 1, 1)
+
+    disp = afwDisplay.Display(fig)
+    disp.scale('asinh', 'zscale', Q=2)
+    #disp.scale('linear', 'minmax')
+    #disp.scale('asinh', 5, 7000, Q=2)
+
+    disp.setImageColormap('viridis' if True else 'gray')
+    dataType = "postISRCCD"
+
+
+    # use that  to limit what's shown
+    #detectorNameList = ['R22_S11', 'R22_S20', 'R22_S10']# , 'R34_S20'] 
+
+    # set to None to plot everything
     
+    mos = cameraGeomUtils.showCamera(camera,
+                                 cameraGeomUtils.ButlerImage(butler, dataType, 
+                                                             instrument=instrument, 
+                                                             exposure=exposure,
+                                                             verbose=True),
+                                 binSize=binSize, 
+                                 detectorNameList=detectorNameList, 
+                                 display=disp, overlay=True, title=f'{instrument}' )
+
+    disp.show_colorbar(False)
+    ax.axis("off")
+
+    #plt.savefig('LsstCam_letter_entire_tiledF_wfs.png', bbox_inches='tight')
+    return mos   
